903260e86fa4c3bce2cb3d481c1bf6a2c25ffc53
Websponsive <e.gangan2005@gmail.com>
2022-12-10 20:49:06 +0200

    Patch 0.5
    -Edited the benefits section to look better aesthetically, and optimized it for different screen sizes

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-function handleMouseMovement (e) {
-    const {currentTarget: target} = e;
+function handleMouseMovement (w) {
+    const {currentTarget: target} = w;
 
     const rect = target.getBoundingClientRect(),
-          x = e.clientX - rect.left,
-          y = e.clientY - rect.top;
+          x = w.clientX - rect.left,
+          y = w.clientY - rect.top;
 
     target.style.setProperty("--mouse-x", `${x}px`);
     target.style.setProperty("--mouse-y", `${y}px`);
 }
 
 for(const benefit of document.querySelectorAll(".benefit")) {
-    benefit.onmousemove = e => handleMouseMovement(e);
+    benefit.onmousemove = w => handleMouseMovement(w);
 }
