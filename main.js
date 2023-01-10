d376b3efe01d53907a34fa0b6d02ca6668ffc94f
Websponsive <e.gangan2005@gmail.com>
2023-01-10 21:59:10 +0200

    Patch 0.14
    -Optimized navigation bar to look less cluttered on mobile by hiding options in a dropdown menu
    -Changed 2022 in the footer to 2023

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const contentOptions = {
     rootMargin: "-150px 0px -150px 0px"
 }
 
+
 //Navbar fade-in animations
 
 const navbar = document.querySelector('.navbar');
@@ -22,6 +23,26 @@ const navObserver = new IntersectionObserver((elements) => {
 }, navOptions);
 
 navObserver.observe(introSection);
+
+
+//Navbar collapse functionality
+const menuButton = document.querySelector('.nav-menu-button');
+const navbarList = document.querySelector('.navbar-list'); 
+menuButton.addEventListener('click', () => {
+    navbarList.classList.toggle('hidden');
+    navbarList.classList.toggle('visible');
+    if(!navbar.classList.contains('nav-scrolled')){
+        navbar.classList.toggle('nav-scrolled');
+    }
+
+    let isOpen = menuButton.getAttribute('aria-expanded');
+    if(isOpen === 'false'){
+        menuButton.setAttribute('aria-expanded', 'true');
+    } else {
+        menuButton.setAttribute('aria-expanded', 'false');
+    }
+});
+
 
 //Fade-in animations for content
 
