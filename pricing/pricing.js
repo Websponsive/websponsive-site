ea071e1dc4c2e5c49f989cd3bca6cdf89eaeb06e
Websponsive <e.gangan2005@gmail.com>
2023-01-15 14:42:47 +0200

    Patch 0.16
    -Finished subscription plans

--- pricing/pricing.js
+++ pricing/pricing.js
@@ -1,14 +1,11 @@
 //Navbar collapse functionality
 
-const menuButton = document.querySelector('.nav-menu-button');
+const menuButton = document.querySelector('.menu-button');
 const navbarList = document.querySelector('.navbar-list'); 
 menuButton.addEventListener('click', () => {
     navbarList.classList.toggle('hidden');
     navbarList.classList.toggle('visible');
-    if(!navbar.classList.contains('nav-scrolled')){
-        navbar.classList.toggle('nav-scrolled');
-    }
-
+    console.log('gigel')
     let isOpen = menuButton.getAttribute('aria-expanded');
     if(isOpen === 'false'){
         menuButton.setAttribute('aria-expanded', 'true');
