76d31e773515dae07fd2c073ced0216dd68be406
Websponsive <e.gangan2005@gmail.com>
2023-01-27 20:11:22 +0200

    Patch 0.21
    -Added cart functionalities to main page
    -Cart now remembers input values

--- main.js
+++ main.js
@@ -4,6 +4,39 @@ const navOptions = {
 const contentOptions = {
     rootMargin: "-150px 0px -150px 0px"
 }
+const cartButton = document.querySelector('#cart-button');
+const cart = document.querySelector('.cart');
+
+//Navbar collapse functionality
+const menuButton = document.querySelector('#menu-button');
+const navbarList = document.querySelector('.navbar-list'); 
+menuButton.addEventListener('click', () => {
+    navbarList.classList.toggle('nav-hidden');
+    navbarList.classList.toggle('nav-visible');
+    cart.classList.remove('cart-visible');
+    cart.classList.add('cart-hidden');
+    
+    if(!navbar.classList.contains('nav-scrolled')){
+        navbar.classList.toggle('nav-scrolled');
+    }
+    
+    let isOpen = menuButton.getAttribute('aria-expanded');
+    if(isOpen === 'false'){
+        menuButton.setAttribute('aria-expanded', 'true');
+    } else {
+        menuButton.setAttribute('aria-expanded', 'false');
+    }
+});
+
+//Cart display functionality
+
+cartButton.addEventListener('click', () => {
+    cart.classList.toggle('cart-hidden');
+    cart.classList.toggle('cart-visible');
+    navbarList.classList.remove('nav-visible');
+    navbarList.classList.add('nav-hidden');
+    menuButton.setAttribute('aria-expanded', 'false');
+});
 
 
 //Navbar fade-in animations
@@ -25,23 +58,6 @@ const navObserver = new IntersectionObserver((elements) => {
 navObserver.observe(introSection);
 
 
-//Navbar collapse functionality
-const menuButton = document.querySelector('#menu-button');
-const navbarList = document.querySelector('.navbar-list'); 
-menuButton.addEventListener('click', () => {
-    navbarList.classList.toggle('hidden');
-    navbarList.classList.toggle('visible');
-    if(!navbar.classList.contains('nav-scrolled')){
-        navbar.classList.toggle('nav-scrolled');
-    }
-
-    let isOpen = menuButton.getAttribute('aria-expanded');
-    if(isOpen === 'false'){
-        menuButton.setAttribute('aria-expanded', 'true');
-    } else {
-        menuButton.setAttribute('aria-expanded', 'false');
-    }
-});
 
 
 //Fade-in animations for content
@@ -74,11 +90,133 @@ for(const benefit of document.querySelectorAll(".benefit")) {
 }
 function handleMouseMovement (w) {
     const {currentTarget: target} = w;
-
+    
     const rect = target.getBoundingClientRect(),
-          x = w.clientX - rect.left,
-          y = w.clientY - rect.top;
-
+    x = w.clientX - rect.left,
+    y = w.clientY - rect.top;
+    
     target.style.setProperty("--mouse-x", `${x}px`);
     target.style.setProperty("--mouse-y", `${y}px`);
 }
+
+/*************** Cart functionalities *******************/
+
+const prices = {
+    essential : 129,
+    value : 249,
+    premium : 499,
+    none : 0,
+}
+
+addOnsState();
+
+const planSelect = document.querySelector('.cart-plan-input');
+const planTitle = document.querySelector('.cart-plan-title');
+const planPrice = document.querySelector('.cart-plan-price');
+const cartTotal = document.querySelector('.cart-total');
+
+//Update plan value from the localstorage
+let selectedPlan = window.localStorage.getItem('plan');
+if (!(selectedPlan === null)) {
+    cartTotal.innerText = window.localStorage.getItem('total');
+    if (selectedPlan === 'essential'){
+        planTitle.innerText = 'Essential';
+        planPrice.innerText = prices.essential;
+        planSelect.value = 'essential';
+    } else if (selectedPlan === 'value') {
+        planTitle.innerText = 'Value';
+        planPrice.innerText = prices.value;
+        planSelect.value = 'value';
+    } else if (selectedPlan === 'premium') {
+        planTitle.innerText = 'Premium';
+        planPrice.innerText = prices.premium;
+        planSelect.value = 'premium';
+    } else if (selectedPlan === 'none') {
+        planTitle.innerText = 'No plan selected';
+        planPrice.innerText = prices.none;
+        planSelect.value = 'none';
+    }
+}
+
+//Plan selector from cart itself
+planSelect.addEventListener('input', () => {
+    if (planSelect.value === 'essential'){
+        planTitle.innerText = 'Essential';
+        planPrice.innerText = prices.essential;
+        window.localStorage.setItem('plan', 'essential');
+    } else if (planSelect.value === 'value') {
+        planTitle.innerText = 'Value';
+        planPrice.innerText = prices.value;
+        window.localStorage.setItem('plan', 'value');
+    } else if (planSelect.value === 'premium') {
+        planTitle.innerText = 'Premium';
+        planPrice.innerText = prices.premium;
+        window.localStorage.setItem('plan', 'premium');
+    } else if (planSelect.value === 'none') {
+        planTitle.innerText = 'No plan selected';
+        planPrice.innerText = prices.none;
+        window.localStorage.setItem('plan', 'none');
+    }
+    
+    updateCartTotal();
+});
+//Add-ons quantity checker
+function quantityEvents() {
+    const cartQuantities = document.querySelectorAll('.cart-quantity-input');
+    cartQuantities.forEach((item) => {
+        item.addEventListener('input', () => {
+            if (isNaN(item.value) || item.value < 0){
+                item.value = 0;
+            } else if (item.value >= 1000) {
+                item.value = 999;
+            }
+            updateCartTotal();
+        });
+    });
+}
+
+
+//Add-ons display function
+function addOnsState() {
+    const addOnsSection = document.querySelector('.cart-add-ons');
+    if(addOnsSection.dataset.pages === 'no' && addOnsSection.dataset.report === 'no') {
+        addOnsSection.style.display = 'none';
+    } else {
+        addOnsSection.style.display = 'block';
+    }
+}
+
+
+//Cart total update function
+function updateCartTotal(){
+    let total = 0;
+    let planElementPrice;
+
+    planElementPrice = document.querySelector('.cart-plan-price').innerText;
+    planElementPrice = 0;
+    
+    total += Number(planElementPrice);
+
+
+    const addOns = document.querySelectorAll('.add-on-element');
+    addOns.forEach((item) => {
+        const addOnPrice = Number(item.querySelector('.cart-item-price').innerText);
+        const addOnQuantity = Number(item.querySelector('.cart-quantity-input').value);
+        total += addOnPrice * addOnQuantity;
+    });
+
+    cartTotal.innerText = total;
+}
+
+//Remove buttons functionality
+function removeButtonsEvents() {
+    const removeButtons = document.querySelectorAll('.cart-remove-button');
+    removeButtons.forEach( (button) => {
+        button.addEventListener('click', (event) => {
+            event.target.parentNode.remove();
+            updateCartTotal();
+            addOnsState();
+        });
+    });
+}
+
--- pricing/pricing.js
+++ pricing/pricing.js
@@ -20,17 +20,7 @@ menuButton.addEventListener('click', () => {
     }
 });
 
-//Cart display functionality
-
-cartButton.addEventListener('click', () => {
-    cart.classList.toggle('cart-hidden');
-    cart.classList.toggle('cart-visible');
-    navbarList.classList.remove('nav-visible');
-    navbarList.classList.add('nav-hidden');
-    menuButton.setAttribute('aria-expanded', 'false');
-});
-
-//Cart functionalities
+/*************** Cart functionalities *******************/
 
 const prices = {
     essential : 129,
@@ -44,30 +34,63 @@ addOnsState();
 const planSelect = document.querySelector('.cart-plan-input');
 const planTitle = document.querySelector('.cart-plan-title');
 const planPrice = document.querySelector('.cart-plan-price');
+const cartTotal = document.querySelector('.cart-total');
+let total;
+
+
+//Update plan value from the localstorage
+let selectedPlan = window.localStorage.getItem('plan');
+if (!(selectedPlan === null)) {
+    cartTotal.innerText = window.localStorage.getItem('total');
+    if (selectedPlan === 'essential'){
+        planTitle.innerText = 'Essential';
+        planPrice.innerText = prices.essential;
+        planSelect.value = 'essential';
+    } else if (selectedPlan === 'value') {
+        planTitle.innerText = 'Value';
+        planPrice.innerText = prices.value;
+        planSelect.value = 'value';
+    } else if (selectedPlan === 'premium') {
+        planTitle.innerText = 'Premium';
+        planPrice.innerText = prices.premium;
+        planSelect.value = 'premium';
+    } else if (selectedPlan === 'none') {
+        planTitle.innerText = 'No plan selected';
+        planPrice.innerText = prices.none;
+        planSelect.value = 'none';
+    }
+}
+
+
+//Plan selector from cart itself
 planSelect.addEventListener('input', () => {
     if (planSelect.value === 'essential'){
         planTitle.innerText = 'Essential';
         planPrice.innerText = prices.essential;
+        window.localStorage.setItem('plan', 'essential');
     } else if (planSelect.value === 'value') {
         planTitle.innerText = 'Value';
         planPrice.innerText = prices.value;
+        window.localStorage.setItem('plan', 'value');
     } else if (planSelect.value === 'premium') {
         planTitle.innerText = 'Premium';
         planPrice.innerText = prices.premium;
+        window.localStorage.setItem('plan', 'premium');
     } else if (planSelect.value === 'none') {
         planTitle.innerText = 'No plan selected';
         planPrice.innerText = prices.none;
+        window.localStorage.setItem('plan', 'none');
     }
     
     updateCartTotal();
 });
-
+//Add-ons quantity checker
 function quantityEvents() {
     const cartQuantities = document.querySelectorAll('.cart-quantity-input');
     cartQuantities.forEach((item) => {
         item.addEventListener('input', () => {
-            if (isNaN(item.value) || item.value <= 0){
-                item.value = 1;
+            if (isNaN(item.value) || item.value < 0){
+                item.value = 0;
             } else if (item.value >= 1000) {
                 item.value = 999;
             }
@@ -76,6 +99,8 @@ function quantityEvents() {
     });
 }
 
+
+//Plan-selecter buttons from the content
 const essentialButton = document.querySelector('.essential-button');
 const valueButton = document.querySelector('.value-button');
 const premiumButton = document.querySelector('.premium-button');
@@ -99,7 +124,7 @@ premiumButton.addEventListener('click', () => {
     updateCartTotal();
 });
 
-
+//Add-ons-selecter buttons from the content
 const pagesButton = document.querySelector('.pages-button');
 const reportButton = document.querySelector('.report-button');
 pagesButton.addEventListener('click', () => {
@@ -111,10 +136,10 @@ pagesButton.addEventListener('click', () => {
     } else {
         newElement.innerHTML = 
         `<div class="cart-item add-on-element">
-            <p class="cart-item-title paragraph dark">3 extra pages</p>
-            <p class="paragraph dark">$<span class="cart-item-price">39</span>/mo</p>
-            <input type="number" class="cart-quantity-input pages-count" value="1">
-            <button class="cart-remove-button light small-text">Remove</button>
+        <p class="cart-item-title paragraph dark">3 extra pages</p>
+        <p class="paragraph dark">$<span class="cart-item-price">39</span>/mo</p>
+        <input type="number" onclick="select()" class="cart-quantity-input pages-count" value="1">
+        <button class="cart-remove-button light small-text">Remove</button>
         </div>`;
         addOnsSection.append(newElement);
         addOnsSection.dataset.pages = 'yes';
@@ -125,18 +150,18 @@ pagesButton.addEventListener('click', () => {
     addOnsState();
 });
 reportButton.addEventListener('click', () => {
-    let newElement = document.createElement('div');
     const addOnsSection = document.querySelector('.cart-add-ons');
     if (addOnsSection.dataset.report === 'yes') {
         const reportCount = document.querySelector('.report-count');
         reportCount.value = Number(reportCount.value) + 1;
     } else {
+        let newElement = document.createElement('div');
         newElement.innerHTML = 
         `<div class="cart-item add-on-element">
-            <p class="cart-item-title paragraph dark">Monthly report</p>
-            <p class="paragraph dark">$<span class="cart-item-price">29</span>/mo</p>
-            <input type="number" class="cart-quantity-input report-count" value="1">
-            <button class="cart-remove-button light small-text">Remove</button>
+        <p class="cart-item-title paragraph dark">Monthly report</p>
+        <p class="paragraph dark">$<span class="cart-item-price">29</span>/mo</p>
+        <input type="number" onclick="select()" class="cart-quantity-input report-count" value="1">
+        <button class="cart-remove-button light small-text">Remove</button>
         </div>`;
         addOnsSection.append(newElement);
         addOnsSection.dataset.report = 'yes';
@@ -147,6 +172,8 @@ reportButton.addEventListener('click', () => {
     addOnsState();
 }); 
 
+
+//Add-ons display function
 function addOnsState() {
     const addOnsSection = document.querySelector('.cart-add-ons');
     if(addOnsSection.dataset.pages === 'no' && addOnsSection.dataset.report === 'no') {
@@ -156,32 +183,27 @@ function addOnsState() {
     }
 }
 
+
+//Cart total update function
 function updateCartTotal(){
-    let total = 0;
-    const cartTotal = document.querySelector('.cart-total');
     let planElementPrice;
-
-    try {
-        planElementPrice = document.querySelector('.cart-plan-price').innerText;
-    }
-    catch {
-        planElementPrice = 0;
-    }
-    
+    total = 0;
+    planElementPrice = document.querySelector('.cart-plan-price').innerText;
     total += Number(planElementPrice);
-
-
+    
+    
     const addOns = document.querySelectorAll('.add-on-element');
     addOns.forEach((item) => {
         const addOnPrice = Number(item.querySelector('.cart-item-price').innerText);
         const addOnQuantity = Number(item.querySelector('.cart-quantity-input').value);
         total += addOnPrice * addOnQuantity;
     });
-
+    
     cartTotal.innerText = total;
+    window.localStorage.setItem('total', `${total}`);
 }
 
-
+//Remove buttons functionality
 function removeButtonsEvents() {
     const removeButtons = document.querySelectorAll('.cart-remove-button');
     removeButtons.forEach( (button) => {
@@ -192,3 +214,15 @@ function removeButtonsEvents() {
         });
     });
 }
+
+//Cart display functionality
+
+cartButton.addEventListener('click', () => {
+    cart.classList.toggle('cart-hidden');
+    cart.classList.toggle('cart-visible');
+    navbarList.classList.remove('nav-visible');
+    navbarList.classList.add('nav-hidden');
+    menuButton.setAttribute('aria-expanded', 'false');
+    updateCartTotal();
+    cartTotal.innerText = total;
+});
