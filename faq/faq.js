2e58676358022333e62e5b46e2700b2a9f05764e
Websponsive <e.gangan2005@gmail.com>
2023-05-02 22:15:09 +0300

    Patch 1.16
    -Began work on accessibility
    -Translated FAQ page to romanian
    -Migrated some features to all pages for parity

--- faq/faq.js
+++ faq/faq.js
@@ -40,6 +40,14 @@ darkModeButton.addEventListener('click', () => {
     }
 }) ;
 
+
+// Questions drop down
+const questions = document.querySelectorAll('.question');
+questions.forEach((q) => {
+    q.addEventListener('click', () => {
+        q.classList.toggle('dropped')
+    })
+});
 /*************** Cart functionalities *******************/
 
 const prices = {
